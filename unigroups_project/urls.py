1d280490e892e601e315c5fcc9ad5db8bdb2a2cd
coder <wa985927@gmail.com>
2026-05-22 15:16:19 +0300

    new test 2

--- unigroups_project/urls.py
+++ unigroups_project/urls.py
@@ -8,14 +8,8 @@ def health(request):
 urlpatterns = [
     path('health/', health),
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('api/auth/',          include('users.urls')),
     path('api/groups/',        include('groups.urls')),
     path('api/chat/',          include('chat.urls')),
     path('api/notifications/', include('notifications.urls')),
-=======
-    path('api/auth/',   include('users.urls')),
-    path('api/groups/', include('groups.urls')),
-    path('api/chat/',   include('chat.urls')),
->>>>>>> 63a7da21ebd8ec983cf9ca698be62c4cc76c5803
 ]
